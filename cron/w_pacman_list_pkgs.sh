f557629c9df502d8628a40a3c2d8f3c248f2d851
antcc <antoniocoin07@gmail.com>
2017-02-12 18:03:52 +0100

    Fix pacman cron file

--- cron/w_pacman_list_pkgs.sh
+++ cron/w_pacman_list_pkgs.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
-# List installed packages & AUR and overwrite lists in 
-.dotfiles/arch/packages
+# List installed packages & AUR and overwrite lists in .dotfiles/arch/packages
 
 pacman -Q > ~/.dotfiles/arch/packages/pacman_list.txt
 pacman -Qm > ~/.dotfiles/arch/packages/aur_list.txt
