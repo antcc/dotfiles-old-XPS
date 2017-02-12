0fc2fb94c51b1b72a529b366baa82bd62722df6d
antcc <antoniocoin07@gmail.com>
2017-02-12 17:57:14 +0100

    Improve install.conf

--- cron/w_pacman_list_pkgs.sh
+++ cron/w_pacman_list_pkgs.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
-# List installed packages & AUR and overwrite lists in .dotfiles/arch/packages
+# List installed packages & AUR and overwrite lists in 
+.dotfiles/arch/packages
 
 pacman -Q > ~/.dotfiles/arch/packages/pacman_list.txt
 pacman -Qm > ~/.dotfiles/arch/packages/aur_list.txt
