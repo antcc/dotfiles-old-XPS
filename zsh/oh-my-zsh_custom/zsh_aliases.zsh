bec9d483062f5ef71553013b1518d3962960b388
antcc <antoniocoin07@gmail.com>
2017-02-12 14:27:09 +0100

    List installed packages pacman

--- zsh/oh-my-zsh_custom/zsh_aliases.zsh
+++ zsh/oh-my-zsh_custom/zsh_aliases.zsh
@@ -3,4 +3,9 @@ alias makepkgt='mkdir -p /tmp/makepkg && BUILDIR=/tmp/makepkg makepkg -si'
 
 # Cower download to a specific dir
 alias cowerd='cower -t ~/Descargas/AUR'
+
+# Custom diff
 alias diff='diff --color -y --suppress-common-lines'
+
+# Emacs on terminal mode
+alias emacs='emacs -nw'
