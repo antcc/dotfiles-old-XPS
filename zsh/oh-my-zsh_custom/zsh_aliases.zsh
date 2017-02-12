30af74c61e2454b203dd8b9c6b70144e0ccccc3b
antcc <antoniocoin07@gmail.com>
2017-02-12 13:30:21 +0100

    Custom files for oh-my-zsh

---
+++ zsh/oh-my-zsh_custom/zsh_aliases.zsh
@@ -0,0 +1,6 @@
+# Building PKGBUILD in /tmp
+alias makepkgt='mkdir -p /tmp/makepkg && BUILDIR=/tmp/makepkg makepkg -si'
+
+# Cower download to a specific dir
+alias cowerd='cower -t ~/Descargas/AUR'
+alias diff='diff --color -y --suppress-common-lines'
