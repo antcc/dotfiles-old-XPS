a8e7b536fbdf40b70583081a5e78f4995b13a186
antcc <antoniocoin07@gmail.com>
2017-02-22 13:11:50 +0100

    Añade netbeans; añade script en proceso para monitores externos

---
+++ bin/external_monitor.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+# Change user settings depending on whether an external monitor is connected.
+
+#----- Enable/Disable GNOME extensions -----#
+
+# Hide top-bar
+# gnome-shell-extension-tool -e (-d) NOMBRE 
+
+# Always show dock in full width
+
+#----- Set font scaling -----#
+
+#xrandr ...
---
+++ emacs/emacs.d/elpa/htmlize-1.51/htmlize-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "htmlize" "1.51" "Convert buffer text and decorations to HTML." 'nil :keywords '("hypermedia" "extensions"))
