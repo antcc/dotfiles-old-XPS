d31dc2c5b3c77f33aa7e499c0c6ae13147334a84
antcc <antoniocoin07@gmail.com>
2021-09-19 13:17:18 +0200

    update

--- bin/check-battery.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-BAT=`upower -i $(upower -e | grep BAT) | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//`
-if [ $BAT -le 15 ]; then
-    notify-send --urgency critical "¡Batería baja!" "$BAT %"
-fi
-
