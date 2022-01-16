12d93d0f944eadc77515d7226fdaba629f59e7c6
antcc <antoniocoin07@gmail.com>
2022-01-17 00:00:04 +0100

    update

--- bin/lock.sh
+++ bin/lock.sh
@@ -6,9 +6,9 @@
 THRESHOLD=55
 IMAGE=$(mktemp).png
 scrot $IMAGE
-COLOR=`convert $IMAGE -colorspace hsb -resize 1x1 txt:- | egrep -o '[0-9]{1,2}\.([0-9]){0,8}%)' | egrep -ow '[0-9]{1,2}'`;
+COLOR=(`convert $IMAGE -colorspace hsb -resize 1x1 txt:- | egrep -o '[0-9]{1,2}\.([0-9]){0,8}%)' | egrep -ow '[0-9]{1,2}'`)
 
-if [ "$COLOR" -lt "$THRESHOLD" ]; then
+if [ ${COLOR[0]} -lt $THRESHOLD ]; then
     O='#ffffffff'
     C='#ffffff7c'
     T='#000000ff'
@@ -24,36 +24,36 @@ W='#880000bb'  # wrong
 V='#ffffff00'  # verifying
 
 i3lock \
---insidevercolor=$C   \
---ringvercolor=$V     \
+--insidever-color=$C   \
+--ringver-color=$V     \
 \
---insidewrongcolor=$C \
---ringwrongcolor=$W   \
+--insidewrong-color=$C \
+--ringwrong-color=$W   \
 \
---insidecolor=$B      \
---ringcolor=$D        \
---linecolor=00000000  \
---separatorcolor=$D   \
+--inside-color=$B      \
+--ring-color=$D        \
+--line-color=00000000  \
+--separator-color=$D   \
 \
---datecolor=$O        \
---timecolor=$O        \
---keyhlcolor=ffffff80 \
---bshlcolor=$W        \
+--date-color=$O        \
+--time-color=$O        \
+--keyhl-color=ffffff80 \
+--bshl-color=$W        \
 \
 --blur=5              \
 --clock               \
 --composite           \
 --radius=100          \
 --ignore-empty-password \
---timestr="%H:%M"  \
---datestr="%A %e %b"  \
+--time-str="%H:%M"  \
+--date-str="%A %e %b"  \
 --pass-screen-keys    \
 --pass-power-keys     \
 --pass-media-keys     \
 \
---veriftext="Verifying..."      \
---wrongtext="Wrong!"            \
---datesize=20                   \
---timesize=35                   \
+--verif-text="Verifying..."      \
+--wrong-text="Wrong!"            \
+--date-size=20                   \
+--time-size=35                   \
 
 rm $IMAGE
